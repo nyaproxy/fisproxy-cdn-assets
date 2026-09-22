c1a493cf3a50f8d3896efe962bc9c095ca5eb225
f1sunia <289944096+F1sunia@users.noreply.github.com>
2026-09-22 12:34:51 +0000

    shop: publish shop-d68450a

---
+++ shop/assets/skeleton-CGBO-kMX.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-BZBrc8-_.js";import{a as t}from"./dist-Dq7K5Avp.js";var n=e();function r({className:e,...r}){return(0,n.jsx)(`div`,{"data-slot":`skeleton`,className:t(`animate-pulse rounded-md bg-muted`,e),...r})}export{r as t};
